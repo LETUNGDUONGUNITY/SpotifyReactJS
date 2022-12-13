c69fc818188eeffee7684c6b9107af804025d06b
LETUNGDUONGUNITY <102021904+LETUNGDUONGUNITY@users.noreply.github.com>
2022-12-13 21:46:33 +0700

    Spotify Project

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import im1 from './ImagesFolder/icon.jpg'
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
+export default function Navbar() {
+    return (
+        <div className='h-24 text-white text-center leading-[6rem] text-3xl bg-green-600'>       
+            ZINGMp3
+            
+        </div>
+    )
+}
